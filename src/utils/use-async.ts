0724d2765058ae8174940c644962a7435a214370
lizheng <lizheng40@lenovo.com>
2022-08-24 20:01:48 +0800

    feat: 用useCallback来阻止重新生成

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { useMountedRef } from 'utils';
 
 type State<D> = {
@@ -29,51 +29,55 @@ export const useAsync = <D>(
   const mountedRef = useMountedRef();
   const [retry, setRetry] = useState(() => () => {});
   // 成功时设置data
-  const setData = (data: D) =>
-    setState({
-      data,
-      stat: 'success',
-      error: null,
-    });
+  const setData = useCallback(
+    (data: D) =>
+      setState({
+        data,
+        stat: 'success',
+        error: null,
+      }),
+    []
+  );
   // 失败的时候设置data
-  const setError = (error: Error) => {
+  const setError = useCallback((error: Error) => {
     setState({
       data: null,
       stat: 'error',
       error,
     });
-  };
+  }, []);
   // run-触发异步请求
-  const run = (
-    promise: Promise<D>,
-    retryConfig?: { retry: () => Promise<D> }
-  ) => {
-    if (!promise || !promise.then) {
-      throw new Error('请传入Promise类型数据');
-    }
-    setRetry(() => () => {
-      console.log('set retry');
-      retryConfig?.retry && run(retryConfig.retry(), retryConfig);
-    });
-    setState({
-      ...state,
-      stat: 'loading',
-    });
-    return promise
-      .then((data) => {
-        if (mountedRef.current) {
-          setData(data);
-        }
-        return data;
-      })
-      .catch((err) => {
-        // catch会消化异常，如果不主动抛出，外面是接受不到异常的。
-        if (config.throwOnError) {
-          return Promise.reject(err);
-        }
-        return err;
+  const run = useCallback(
+    (promise: Promise<D>, retryConfig?: { retry: () => Promise<D> }) => {
+      if (!promise || !promise.then) {
+        throw new Error('请传入Promise类型数据');
+      }
+      setRetry(() => () => {
+        console.log('set retry');
+        retryConfig?.retry && run(retryConfig.retry(), retryConfig);
       });
-  };
+      // setState({
+      //   ...state,
+      //   stat: 'loading',
+      // });
+      setState((prevState) => ({ ...prevState, stat: 'loading' }));
+      return promise
+        .then((data) => {
+          if (mountedRef.current) {
+            setData(data);
+          }
+          return data;
+        })
+        .catch((err) => {
+          // catch会消化异常，如果不主动抛出，外面是接受不到异常的。
+          if (config.throwOnError) {
+            return Promise.reject(err);
+          }
+          return err;
+        });
+    },
+    [config.throwOnError, mountedRef, setData]
+  );
   return {
     isIdle: state.stat === 'idle',
     isLoading: state.stat === 'loading',
