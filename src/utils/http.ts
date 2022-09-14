5d06b43ea1365cdea4f95f4fa39de7ed7dec85db
lizheng <lizheng40@lenovo.com>
2022-09-14 11:44:33 +0800

    feat: 暂存(practice)

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -13,7 +13,7 @@ interface Config extends RequestInit {
 // axios的表现和fetch不一样，axios可以直接在返回状态不是200的时候返回异常报错
 
 export const http = async (
-  endpoint: string,
+  url: string,
   { data, token, headers, ...customConfig }: Config = {}
 ) => {
   const config = {
@@ -26,35 +26,33 @@ export const http = async (
   };
 
   if (config.method.toUpperCase() === 'GET') {
-    endpoint += `?${qs.stringify(data)}`;
+    url += `?${qs.stringify(data)}`;
   } else {
     config.body = JSON.stringify(data || {});
   }
 
   // axios 和 fetch 的表现不一样，axios可以直接在返回状态不为2xx的时候抛出异常
-  return window
-    .fetch(`${apiUrl}/${endpoint}`, config)
-    .then(async (response) => {
-      if (response.status === 401) {
-        await auth.logout();
-        window.location.reload();
-        return Promise.reject({ message: '请重新登录' });
-      }
-      const data = await response.json();
-      if (response.ok) {
-        return data;
-      } else {
-        return Promise.reject(data);
-      }
-    });
+  return window.fetch(`${apiUrl}/${url}`, config).then(async (response) => {
+    if (response.status === 401) {
+      await auth.logout();
+      window.location.reload();
+      return Promise.reject({ message: '请重新登录' });
+    }
+    const data = await response.json();
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(data);
+    }
+  });
 };
 
 export const useHttp = () => {
   const { user } = useAuth();
   // utility type 的用法：用泛型给它传入一个其他类型，然后utility type对这个类型进行某种操作
   return useCallback(
-    (...[endpoint, config]: Parameters<typeof http>) =>
-      http(endpoint, { ...config, token: user?.token }),
+    (...[url, config]: Parameters<typeof http>) =>
+      http(url, { ...config, token: user?.token }),
     [user?.token]
   );
 };
